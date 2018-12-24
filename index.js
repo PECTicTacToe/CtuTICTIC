5e45236258a4c868deda20171fe830204186a8ac
Abhinav <hemrajjain1999@gmail.com>
2018-12-25 01:08:56 +0530

    initial

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 const axios = require("axios");
 const url = "https://reqres.in/api/users?page=2";
 app.get("/", async (req, res) => {
-  const response = await axios.get(url);
-  res.send({ data: response.data });
+  const { data } = await axios.get(url);
+  res.send({ data });
 });
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server started at ${PORT}`));
